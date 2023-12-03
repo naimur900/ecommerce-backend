6843a1c97902a636536b4a00087deced42cffbfb
Naimur Rahman <naimur.rahman900@gmail.com>
2023-12-03 17:20:36 +0600

    connected database

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
 
 const app = express()
 const port = process.env.PORT
+
+// Connect database
+mongoose.connect(process.env.MONGOURI).then(()=>{
+    console.log("DB connected");
+}).catch((err)=>{
+    console.log(err.message);
+})
 
 app.get("/",(req,res)=>{
     res.status(200).json({
