7f50c899f9c48aad95fa632db23c516b4639072a
Naimur Rahman <naimur.rahman900@gmail.com>
2023-12-07 17:21:48 +0600

    added shop controller

---
+++ controllers/shopController.js
@@ -0,0 +1,104 @@
+const Shop = require('../model/Shop')
+
+
+const insertShop = async (req, res)=>{
+    try {
+    const shop = new Shop(req.body)
+        await shop.save()
+    } catch (error) {
+        res.status(400).json({
+            status: false,
+            Message: error.Message
+        })
+        
+    }
+}
+
+const getShopById = async (req,res) => {
+    try {
+        const shopId = req.params.shopId;
+        const shop = await Shop.findById(shopId);
+        if(!shop){
+            res.status(404).json({
+                status: false,
+                message: "Shop not found to delete"
+            })
+        }
+        else{
+            res.status(200).json({
+                status: true,
+                message: "Shop deleted successfully",
+                shop: shop
+            })
+
+        }
+        
+    } catch (error) {
+        res.status(404).json({
+            status: false,
+            message: error.message
+        })
+    }
+}
+
+const deleteShopById = async (req,res) => {
+    try {
+        const shopId = req.params.shopId;
+        const shop = await Shop.findById(shopId);
+        if(!shop){
+            res.status(404).json({
+                status: false,
+                message: "Shop not found to delete"
+            })
+        }
+        else{
+            await Shop.findByIdAndDelete(shopId)
+            res.status(200).json({
+                status: true,
+                message: "Shop deleted successfully"
+            })
+
+        }
+        
+    } catch (error) {
+        res.status(404).json({
+            status: false,
+            message: error.message
+        })
+    }
+}
+
+const updateShopById = async (req,res) => {
+    try {
+        const shopId = req.params.shopId;
+        const shop = await Shop.findById(shopId);
+        if(!shop){
+            res.status(404).json({
+                status: false,
+                message: "Shop not found to delete"
+            })
+        }
+        else{
+            const updatedShop = await Shop.findByIdAndUpdate(shopId,req.body,{
+                    new: true,
+                    runValidators: true
+            })
+            res.status(200).json({
+                status: true,
+                message: "Shop deleted successfully",
+                updatedShop: updatedShop
+            })
+
+        }
+        
+    } catch (error) {
+        res.status(404).json({
+            status: false,
+            message: error.message
+        })
+    }
+}
+
+
+
+module.exports = {insertShop, deleteShopById, getShopById, updateShopById}
